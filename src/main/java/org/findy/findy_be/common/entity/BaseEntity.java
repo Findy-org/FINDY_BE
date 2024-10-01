0609f3c2aa309edd456a4e8c2a5b91d4b11607c3
나경호 <hoyana1225@gmail.com>
2024-10-01 16:03:59 +0900

    feat: global superclass entity

---
+++ src/main/java/org/findy/findy_be/common/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package org.findy.findy_be.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(columnDefinition = "TIMESTAMP", name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(columnDefinition = "TIMESTAMP", name = "updated_at")
+	private LocalDateTime updatedAt;
+}
