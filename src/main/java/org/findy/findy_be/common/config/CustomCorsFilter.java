f2af23c444373a8cf87748129c231086efea863b
나경호 <hoyana1225@gmail.com>
2024-10-03 21:56:16 +0900

    refactor: 동적으로 허용할 도메인 리스트 추가

--- src/main/java/org/findy/findy_be/common/config/CustomCorsFilter.java
+++ src/main/java/org/findy/findy_be/common/config/CustomCorsFilter.java
@@ -19,17 +19,25 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CustomCorsFilter implements Filter {
 
 	@Override
-	public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws
-		IOException,
-		ServletException {
+	public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
+		throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse)res;
 		HttpServletRequest request = (HttpServletRequest)req;
+
+		String origin = request.getHeader("Origin");
+
+		if (origin != null && (origin.equals("http://localhost:5173") ||
+			origin.equals("https://nid.naver.com") ||
+			origin.equals("https://kauth.kakao.com"))) {
+			response.setHeader("Access-Control-Allow-Origin", origin);
+		}
+
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 			"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
-		response.setHeader("Access-Control-Expose-Headers", "*, Authorization, Authorization-refresh");
+		response.setHeader("Access-Control-Expose-Headers", "Authorization, Authorization-refresh");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
