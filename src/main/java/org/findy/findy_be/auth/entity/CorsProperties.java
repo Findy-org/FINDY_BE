eccd7080ae8027052022f72a9127b1e30f8752ed
나경호 <hoyana1225@gmail.com>
2024-10-03 21:45:52 +0900

    feat: CORS(Cross-Origin Resource Sharing) 관련 설정을 관리합니다.

---
+++ src/main/java/org/findy/findy_be/auth/entity/CorsProperties.java
@@ -0,0 +1,17 @@
+package org.findy.findy_be.auth.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+	private String allowedOrigins;
+	private String allowedMethods;
+	private String allowedHeaders;
+	private Long maxAge;
+}
+
