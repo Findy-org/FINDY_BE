5f85a95abed789b83557fc329c8902fa80f8b08d
나경호 <hoyana1225@gmail.com>
2024-10-03 21:47:12 +0900

    feat: 제공자가 일치하지 않을 때 발생하는 예외입니다.

---
+++ src/main/java/org/findy/findy_be/common/exception/custom/OAuthProviderMissMatchException.java
@@ -0,0 +1,8 @@
+package org.findy.findy_be.common.exception.custom;
+
+public class OAuthProviderMissMatchException extends RuntimeException {
+
+	public OAuthProviderMissMatchException(String message) {
+		super(message);
+	}
+}
