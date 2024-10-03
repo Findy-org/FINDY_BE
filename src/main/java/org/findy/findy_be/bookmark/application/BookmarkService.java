847df4c067ddd3d8de4d3e089a52fd6392520932
나경호 <hoyana1225@gmail.com>
2024-10-03 23:32:22 +0900

    feat: Bookmark service 생성

---
+++ src/main/java/org/findy/findy_be/bookmark/application/BookmarkService.java
@@ -0,0 +1,24 @@
+package org.findy.findy_be.bookmark.application;
+
+import java.util.List;
+
+import org.findy.findy_be.bookmark.domain.Bookmark;
+import org.findy.findy_be.bookmark.repository.BookmarkRepository;
+import org.findy.findy_be.user.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookmarkService {
+
+	private final BookmarkRepository bookmarkRepository;
+
+	public void initBookmarks(User user) {
+		List<Bookmark> bookmarks = Bookmark.initBookmarks(user);
+		bookmarkRepository.saveAll(bookmarks);
+	}
+}
