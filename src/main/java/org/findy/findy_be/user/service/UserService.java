475a7fa5c947099a0a8233a1754c8db0f3dd1ad6
나경호 <hoyana1225@gmail.com>
2024-10-03 21:45:11 +0900

    feat: 사용자 관련 비즈니스 로직을 처리하는 서비스 클래스입니다. 사용자 생성, 조회, 수정 등을 처리할 수 있습니다.

--- src/main/java/org/findy/findy_be/user/service/UserService.java
+++ src/main/java/org/findy/findy_be/user/service/UserService.java
@@ -1,13 +1,7 @@
 package org.findy.findy_be.user.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.findy.findy_be.user.dto.response.UserResponse;
-import org.findy.findy_be.user.entity.Role;
 import org.findy.findy_be.user.entity.User;
 import org.findy.findy_be.user.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,34 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 public class UserService {
 
 	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
 
 	public User findUser(final Long userId) {
 		return userRepository.findById(userId)
 			.orElseThrow(() -> new IllegalArgumentException("[Error] 사용자를 찾을 수 없습니다."));
-	}
-
-	@Transactional
-	public Long changeRole(final Long userId) {
-		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new IllegalArgumentException("[Error] 사용자를 찾을 수 없습니다."));
-		if (!isAdmin(user)) {
-			user.updateRole(Role.ADMIN);
-			return user.getId();
-		}
-		user.updateRole(Role.USER);
-		return user.getId();
-	}
-
-	public List<UserResponse> findAll() {
-		List<User> users = userRepository.findAll();
-		return users.stream()
-			.map(UserResponse::of)
-			.collect(Collectors.toList());
-	}
-
-	private Boolean isAdmin(User user) {
-		return user.getRole().name().equals("ADMIN");
 	}
 
 }
