e041c002c0c410c76c5623a33d275c5bbe1219a2
나경호 <hoyana1225@gmail.com>
2024-10-03 21:51:55 +0900

    feat: Spring Security에서 사용하는 사용자 정보를 로드하는 서비스입니다.

--- src/main/java/org/findy/findy_be/auth/oauth/service/CustomUserDetailsService.java
+++ src/main/java/org/findy/findy_be/auth/oauth/service/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
-package org.findy.findy_be.auth.service;
+package org.findy.findy_be.auth.oauth.service;
 
 import static org.findy.findy_be.common.exception.ErrorCode.*;
 
-import org.findy.findy_be.auth.UserAdapter;
+import org.findy.findy_be.auth.oauth.entity.UserPrincipal;
 import org.findy.findy_be.user.entity.User;
 import org.findy.findy_be.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,20 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
 	private final UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email).orElseThrow(() ->
-			new UsernameNotFoundException(NOT_FOUND_USER.getMessage()));
-		return new UserAdapter(user);
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findById(Long.valueOf(username))
+			.orElseThrow(() -> new EntityNotFoundException(String.format(NOT_FOUND_USER_BY_ID.getMessage())));
+		if (user == null) {
+			throw new UsernameNotFoundException("Can not find username.");
+		}
+		return UserPrincipal.create(user);
 	}
 }
