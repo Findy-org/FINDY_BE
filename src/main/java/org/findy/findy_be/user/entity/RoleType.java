423ad99d3ad9ab98307ada13cd9b7eff1119b045
나경호 <hoyana1225@gmail.com>
2024-10-03 21:46:45 +0900

    feat: java: 사용자 권한(Role)을 정의하는 Enum 클래스입니다.

---
+++ src/main/java/org/findy/findy_be/user/entity/RoleType.java
@@ -0,0 +1,26 @@
+package org.findy.findy_be.user.entity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public enum RoleType {
+	USER("ROLE_USER", "일반 사용자 권한"),
+	ADMIN("ROLE_ADMIN", "관리자 권한"),
+	GUEST("GUEST", "게스트 권한");
+
+	private final String code;
+	private final String displayName;
+
+	public static RoleType of(String code) {
+		return Arrays.stream(RoleType.values())
+			.filter(r -> r.getCode().equals(code))
+			.findAny()
+			.orElse(GUEST);
+	}
+}
