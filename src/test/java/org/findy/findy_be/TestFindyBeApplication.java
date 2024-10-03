d5bb8b760ca00b5c124ac230183c4798f0ef9ba1
나경호 <hoyana1225@gmail.com>
2024-10-04 01:01:25 +0900

    test: 오류 해결 및 불필요한 공백 제거

--- src/test/java/org/findy/findy_be/TestFindyBeApplication.java
+++ src/test/java/org/findy/findy_be/TestFindyBeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class TestFindyBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(FindyBeApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(FindyBeApplication::main).with(TestContainerConfiguration.class).run(args);
 	}
-
 }
