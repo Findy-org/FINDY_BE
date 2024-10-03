12b0360843d30bd632679cbe51c0978eb44a4e64
나경호 <hoyana1225@gmail.com>
2024-10-03 21:48:15 +0900

    feat: OAuth2 인증 성공 시 실행되는 핸들러입니다. 인증 성공 후 JWT 토큰 발급을 처리할 수 있습니다.

---
+++ src/main/java/org/findy/findy_be/auth/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,148 @@
+package org.findy.findy_be.auth.oauth.handler;
+
+import static org.findy.findy_be.auth.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
+import org.findy.findy_be.auth.oauth.entity.SocialProviderType;
+import org.findy.findy_be.auth.oauth.info.OAuth2UserInfo;
+import org.findy.findy_be.auth.oauth.info.OAuth2UserInfoFactory;
+import org.findy.findy_be.auth.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import org.findy.findy_be.auth.oauth.token.AuthToken;
+import org.findy.findy_be.auth.oauth.token.AuthTokenProvider;
+import org.findy.findy_be.common.config.AppProperties;
+import org.findy.findy_be.common.utils.CookieUtil;
+import org.findy.findy_be.user.entity.RoleType;
+import org.findy.findy_be.user.entity.UserRefreshToken;
+import org.findy.findy_be.user.repository.UserRefreshTokenRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+	private final AuthTokenProvider tokenProvider;
+	private final AppProperties appProperties;
+	private final UserRefreshTokenRepository userRefreshTokenRepository;
+	private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) throws IOException {
+		String targetUrl = determineTargetUrl(request, response, authentication);
+
+		if (response.isCommitted()) {
+			logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+			return;
+		}
+
+		clearAuthenticationAttributes(request, response);
+		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+	}
+
+	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) {
+		Optional<String> redirectUri = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+			.map(Cookie::getValue);
+
+		if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+			throw new IllegalArgumentException(
+				"Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+		}
+
+		String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+		OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken)authentication;
+		SocialProviderType socialProviderType = SocialProviderType.valueOf(
+			authToken.getAuthorizedClientRegistrationId().toUpperCase());
+
+		OidcUser user = ((OidcUser)authentication.getPrincipal());
+		OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(socialProviderType, user.getAttributes());
+		Collection<? extends GrantedAuthority> authorities = ((OidcUser)authentication.getPrincipal()).getAuthorities();
+
+		RoleType roleType = hasAuthority(authorities, RoleType.ADMIN.getCode()) ? RoleType.ADMIN : RoleType.USER;
+
+		Date now = new Date();
+		AuthToken accessToken = tokenProvider.createAuthToken(
+			userInfo.getId(),
+			roleType.getCode(),
+			new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
+		);
+
+		// refresh 토큰 설정
+		long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
+
+		AuthToken refreshToken = tokenProvider.createAuthToken(
+			appProperties.getAuth().getTokenSecret(),
+			new Date(now.getTime() + refreshTokenExpiry)
+		);
+
+		// DB 저장
+		UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserId(userInfo.getId());
+		if (userRefreshToken != null) {
+			userRefreshToken.setRefreshToken(refreshToken.getToken());
+		} else {
+			userRefreshToken = new UserRefreshToken(userInfo.getId(), refreshToken.getToken());
+			userRefreshTokenRepository.saveAndFlush(userRefreshToken);
+		}
+
+		int cookieMaxAge = (int)refreshTokenExpiry / 60;
+
+		CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+		CookieUtil.addCookie(response, REFRESH_TOKEN, refreshToken.getToken(), cookieMaxAge);
+
+		return UriComponentsBuilder.fromUriString(targetUrl)
+			.queryParam("token", accessToken.getToken())
+			.build().toUriString();
+	}
+
+	protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+		super.clearAuthenticationAttributes(request);
+		authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+	}
+
+	private boolean hasAuthority(Collection<? extends GrantedAuthority> authorities, String authority) {
+		if (authorities == null) {
+			return false;
+		}
+
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if (authority.equals(grantedAuthority.getAuthority())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isAuthorizedRedirectUri(String uri) {
+		URI clientRedirectUri = URI.create(uri);
+
+		return appProperties.getOauth2().getAuthorizedRedirectUris()
+			.stream()
+			.anyMatch(authorizedRedirectUri -> {
+				// Only validate host and port. Let the clients use different paths if they want to
+				URI authorizedURI = URI.create(authorizedRedirectUri);
+				if (authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+					&& authorizedURI.getPort() == clientRedirectUri.getPort()) {
+					return true;
+				}
+				return false;
+			});
+	}
+}
+
