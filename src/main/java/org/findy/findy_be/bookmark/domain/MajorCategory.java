7d8d1b348ab473d3c6c7311e7fe716bad7d9d091
나경호 <hoyana1225@gmail.com>
2024-10-03 23:34:00 +0900

    feat: 장소 대분류

---
+++ src/main/java/org/findy/findy_be/bookmark/domain/MajorCategory.java
@@ -0,0 +1,17 @@
+package org.findy.findy_be.bookmark.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Schema(description = "즐겨찾기 대분류", enumAsRef = true)
+@Getter
+@RequiredArgsConstructor
+public enum MajorCategory {
+
+	RESTAURANT("음식점"),
+	TRAVEL_AND_ATTRACTION("여행,명소"),
+	SHOPPING_AND_DISTRIBUTION("쇼핑,유통");
+
+	private final String label;
+}
