6b9a8a731fa85f4f11056c4971c9693c5c20ba39
나경호 <hoyana1225@gmail.com>
2024-10-03 21:44:51 +0900

    feat: 사용자 관련 데이터베이스 작업을 담당하는 인터페이스입니다.

--- src/main/java/org/findy/findy_be/user/repository/UserRepository.java
+++ src/main/java/org/findy/findy_be/user/repository/UserRepository.java
@@ -1,19 +1,8 @@
 package org.findy.findy_be.user.repository;
 
-import java.util.Optional;
-
-import org.findy.findy_be.user.entity.SocialType;
 import org.findy.findy_be.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByOauthId(final String oauthId);
-
-	Optional<User> findByEmail(String email);
-
-	Optional<User> findByNickname(String refreshToken);
-
-	Optional<User> findByRefreshToken(String refreshToken);
-
-	Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String id);
+	User findByUserId(String userId);
 }
