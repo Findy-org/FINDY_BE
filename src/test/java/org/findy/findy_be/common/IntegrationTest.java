ee1550d1fba90c2116e66b8ae3bece81cc572a4b
나경호 <hoyana1225@gmail.com>
2024-10-04 01:01:00 +0900

    test: test 중복 설정 관리

---
+++ src/test/java/org/findy/findy_be/common/IntegrationTest.java
@@ -0,0 +1,31 @@
+package org.findy.findy_be.common;
+
+import org.findy.findy_be.common.config.SecurityConfig;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.transaction.Transactional;
+
+@Import({TestcontainersConfiguration.class, SecurityConfig.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("integration-test")
+@Ignore
+public class IntegrationTest {
+	@Autowired
+	protected MockMvc mvc;
+	@Autowired
+	protected ObjectMapper objectMapper;
+}
---
+++ src/test/java/org/findy/findy_be/common/MockTest.java
@@ -0,0 +1,17 @@
+package org.findy.findy_be.common;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@ActiveProfiles("test")
+@RunWith(MockitoJUnitRunner.class)
+@TestPropertySource(properties = {
+	"web-client.base-url-test=http://localhost:8080",
+	"jwt.secretKey-test=my_test_secret_key"
+})
+@Ignore
+public class MockTest {
+}
