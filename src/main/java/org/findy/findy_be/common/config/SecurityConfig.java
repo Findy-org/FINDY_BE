267272fdc1489fa86973e47df5f6e5806873ff75
나경호 <hoyana1225@gmail.com>
2024-10-03 21:46:20 +0900

    chore: Spring Security 설정을 구성하는 클래스입니다. 인증, 인가 설정과 OAuth2 설정을 정의합니다.

--- src/main/java/org/findy/findy_be/common/config/SecurityConfig.java
+++ src/main/java/org/findy/findy_be/common/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package org.findy.findy_be.common.config;
 
-import org.findy.findy_be.auth.login.filter.CustomJsonUsernamePasswordAuthenticationFilter;
-import org.findy.findy_be.auth.login.handler.LoginFailureHandler;
-import org.findy.findy_be.auth.login.handler.LoginSuccessHandler;
-import org.findy.findy_be.auth.login.service.LoginService;
-import org.findy.findy_be.auth.oauth.handler.OAuth2LoginFailureHandler;
-import org.findy.findy_be.auth.oauth.handler.OAuth2LoginSuccessHandler;
+import java.util.Arrays;
+
+import org.findy.findy_be.auth.entity.CorsProperties;
+import org.findy.findy_be.auth.oauth.filter.TokenAuthenticationFilter;
+import org.findy.findy_be.auth.oauth.handler.OAuth2AuthenticationFailureHandler;
+import org.findy.findy_be.auth.oauth.handler.OAuth2AuthenticationSuccessHandler;
+import org.findy.findy_be.auth.oauth.handler.TokenAccessDeniedHandler;
+import org.findy.findy_be.auth.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
 import org.findy.findy_be.auth.oauth.service.CustomOAuth2UserService;
-import org.findy.findy_be.common.jwt.JwtAuthenticationProcessingFilter;
-import org.findy.findy_be.common.jwt.JwtService;
-import org.findy.findy_be.user.repository.UserRepository;
+import org.findy.findy_be.auth.oauth.service.CustomUserDetailsService;
+import org.findy.findy_be.auth.oauth.token.AuthTokenProvider;
+import org.findy.findy_be.user.repository.UserRefreshTokenRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -19,17 +21,19 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,13 +46,24 @@ public class SecurityConfig {
 	private static final String API_PREFIX = "/api";
 	private static final String ADMIN_API_PREFIX = "/api/admin";
 
-	private final LoginService loginService;
-	private final JwtService jwtService;
-	private final UserRepository userRepository;
-	private final ObjectMapper objectMapper;
-	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
-	private final CustomOAuth2UserService customOAuth2UserService;
+	private final CorsProperties corsProperties;
+	private final AppProperties appProperties;
+	private final AuthTokenProvider tokenProvider;
+	private final CustomUserDetailsService userDetailsService;
+	private final CustomOAuth2UserService oAuth2UserService;
+	private final TokenAccessDeniedHandler tokenAccessDeniedHandler;
+	private final UserRefreshTokenRepository userRefreshTokenRepository;
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring()
+			.requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+			.requestMatchers(new AntPathRequestMatcher("/favicon.ico"))
+			.requestMatchers(new AntPathRequestMatcher("/css/**"))
+			.requestMatchers(new AntPathRequestMatcher("/js/**"))
+			.requestMatchers(new AntPathRequestMatcher("/img/**"))
+			.requestMatchers(new AntPathRequestMatcher("/lib/**"));
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws
@@ -60,24 +75,29 @@ public class SecurityConfig {
 			.sessionManagement(sessionManagement ->
 				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.exceptionHandling(e -> e
-				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				.accessDeniedHandler(tokenAccessDeniedHandler))
 			.authorizeHttpRequests(authorizeHttpRequests ->
 				authorizeHttpRequests
 					.requestMatchers("/favicon.ico/**", "/css/**", "/js/**", "/img/**", "/lib/**")
 					.permitAll()
 					.requestMatchers(new MvcRequestMatcher(introspector, "/index.html"))
 					.permitAll()
-					.requestMatchers(new MvcRequestMatcher(introspector, API_PREFIX + "/"))
+					.requestMatchers(new MvcRequestMatcher(introspector, "/**"))
 					.permitAll()
-					.requestMatchers(new MvcRequestMatcher(introspector, "/login/oauth2/**"))
+					.requestMatchers(new MvcRequestMatcher(introspector, "/oauth2/authorization/**"))
 					.permitAll()
-					.requestMatchers(new MvcRequestMatcher(introspector, API_PREFIX + "/auth/sign-up"))
+					.requestMatchers(new MvcRequestMatcher(introspector, API_PREFIX + "/login"))
 					.permitAll()
-					.requestMatchers(new MvcRequestMatcher(introspector, API_PREFIX + "/auth/sign-in"))
+					.requestMatchers(new MvcRequestMatcher(introspector, API_PREFIX + "/auth/**"))
 					.permitAll()
 					.requestMatchers(new MvcRequestMatcher(introspector, ADMIN_API_PREFIX + "/**"))
 					.hasRole("ADMIN")
 					.requestMatchers(new MvcRequestMatcher(introspector, "/findy/api-docs/**"))
+					.permitAll()
+					.requestMatchers(new MvcRequestMatcher(introspector, "/actuator/**"))
+					.permitAll()
+					.requestMatchers(new MvcRequestMatcher(introspector, "/grafana/**"))
 					.permitAll()
 					.requestMatchers(new MvcRequestMatcher(introspector, "/findy/swagger-ui/index.html"))
 					.permitAll()
@@ -85,62 +105,67 @@ public class SecurityConfig {
 					.permitAll()
 					.requestMatchers(new MvcRequestMatcher(introspector, "/v3/api-docs/**")) // Swagger API docs 접근 허용
 					.permitAll()
-					.anyRequest()
-					.authenticated())
-
+					.anyRequest().authenticated())
 			.oauth2Login(oauth2Configurer ->
 				oauth2Configurer
-					.successHandler(oAuth2LoginSuccessHandler)
-					.failureHandler(oAuth2LoginFailureHandler)
 					.userInfoEndpoint(userInfoEndpointConfig ->
-						userInfoEndpointConfig
-							.userService(customOAuth2UserService)))
+						userInfoEndpointConfig.userService(oAuth2UserService) // UserService 설정
+					)
+					.successHandler(oAuth2AuthenticationSuccessHandler())
+					.failureHandler(oAuth2AuthenticationFailureHandler()));
 
-			// AuthenticationFilter 커스텀 적용
-			.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
-
-			// JWT Filter 후 사용자 인증
-			.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
-	// AuthenticationManager <-> AuthenticatorProvider <-> UserDetailsService <-> UserDetails
+	@Bean
+	public TokenAuthenticationFilter tokenAuthenticationFilter() {
+		return new TokenAuthenticationFilter(tokenProvider);
+	}
+
+	@Bean
+	public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
+		return new OAuth2AuthenticationSuccessHandler(
+			tokenProvider,
+			appProperties,
+			userRefreshTokenRepository,
+			oAuth2AuthorizationRequestBasedOnCookieRepository()
+		);
+	}
+
+	@Bean
+	public OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler() {
+		return new OAuth2AuthenticationFailureHandler(oAuth2AuthorizationRequestBasedOnCookieRepository());
+	}
+
+	@Bean
+	public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+		return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+	}
+
 	@Bean
 	public AuthenticationManager authenticationManager() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setPasswordEncoder(passwordEncoder());
-		provider.setUserDetailsService(loginService);
+		provider.setUserDetailsService(userDetailsService);
 		return new ProviderManager(provider);
 	}
 
 	@Bean
-	public LoginSuccessHandler loginSuccessHandler() {
-		return new LoginSuccessHandler(jwtService, userRepository);
-	}
-
-	@Bean
-	public LoginFailureHandler loginFailureHandler() {
-		return new LoginFailureHandler();
-	}
-
-	@Bean
-	public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
-		CustomJsonUsernamePasswordAuthenticationFilter customFilter =
-			new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
-		customFilter.setAuthenticationManager(authenticationManager());
-		customFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
-		customFilter.setAuthenticationFailureHandler(loginFailureHandler());
-		return customFilter;
-	}
-
-	@Bean
-	public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-		return new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource corsConfigSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
+		corsConfig.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
+		corsConfig.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
+		corsConfig.setAllowCredentials(true);
+		corsConfigSource.registerCorsConfiguration("/**", corsConfig);
+		return corsConfigSource;
 	}
 }
