8af105976552dbad2eebf7dde35365003f19d044
나경호 <hoyana1225@gmail.com>
2024-10-06 12:24:15 +0900

    feat: UserController api presentation

---
+++ src/main/java/org/findy/findy_be/user/api/swagger/UserAPIPresentation.java
@@ -0,0 +1,22 @@
+package org.findy.findy_be.user.api.swagger;
+
+import org.findy.findy_be.common.meta.LoginUser;
+import org.findy.findy_be.user.domain.User;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "User API", description = "사용자 관련 API 입니다.")
+public interface UserAPIPresentation {
+
+	@Operation(summary = "사용자 정보 조회",
+		description = "현재 로그인한 사용자의 프로필 이미지 URL을 조회합니다.",
+		responses = {
+			@ApiResponse(responseCode = "200", description = "사용자 정보 조회 성공"),
+			@ApiResponse(responseCode = "401", description = "인증 실패"),
+			@ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
+		})
+	String getUser(@LoginUser User user);
+}
+
