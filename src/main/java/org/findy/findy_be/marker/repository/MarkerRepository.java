012894e1a9c08889c2b140e49a163a0a6509ec29
나경호 <hoyana1225@gmail.com>
2024-10-10 01:51:50 +0900

    refactor: 중복 marker 예외

--- src/main/java/org/findy/findy_be/marker/repository/MarkerRepository.java
+++ src/main/java/org/findy/findy_be/marker/repository/MarkerRepository.java
@@ -1,7 +1,12 @@
 package org.findy.findy_be.marker.repository;
 
+import java.util.Optional;
+
+import org.findy.findy_be.bookmark.domain.Bookmark;
 import org.findy.findy_be.marker.domain.Marker;
+import org.findy.findy_be.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarkerRepository extends JpaRepository<Marker, Long> {
+	Optional<Marker> findByBookmarkAndPlace(Bookmark bookmark, Place place);
 }
