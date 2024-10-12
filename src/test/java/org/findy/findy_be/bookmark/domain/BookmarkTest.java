a095089e8996a3b98f6ca9e844579d3639887429
나경호 <hoyana1225@gmail.com>
2024-10-12 18:23:16 +0900

    test: Bookmark 새로운 타입 정의 생성 test

--- src/test/java/org/findy/findy_be/bookmark/domain/BookmarkTest.java
+++ src/test/java/org/findy/findy_be/bookmark/domain/BookmarkTest.java
@@ -3,10 +3,8 @@ package org.findy.findy_be.bookmark.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.findy.findy_be.auth.oauth.domain.SocialProviderType;
-import org.findy.findy_be.place.domain.MajorCategory;
 import org.findy.findy_be.user.domain.RoleType;
 import org.findy.findy_be.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,30 +14,40 @@ import org.junit.jupiter.api.Test;
 class BookmarkTest {
 
 	private User user;
-	private Bookmark bookmark;
 
 	@BeforeEach
 	void setUp() {
 		user = User.create("N49sfgdahdKz_fp-223424er1N3D6kd", "나경호", "hoyana@naver.com", "Y",
 			"https://github.com/account", SocialProviderType.NAVER, RoleType.USER,
 			LocalDateTime.now(), LocalDateTime.now());
-		bookmark = Bookmark.of(MajorCategory.RESTAURANT.getLabel(), MajorCategory.RESTAURANT, user);
 	}
 
-	@DisplayName("즐겨찾기 초기화 성공")
+	@DisplayName("Youtube 즐겨찾기 생성 성공")
 	@Test
-	public void 즐겨찾기_초기화_성공() throws Exception {
+	public void Youtube_즐겨찾기_생성_성공() throws Exception {
 		// given
-		List<Bookmark> bookmarks = Bookmark.initBookmarks(user);
+		String name = "걍밍경";
+		String youtuberId = "@iammingki";
 
 		// when
-		List<MajorCategory> majorCategories = List.of(MajorCategory.values());
+		Bookmark bookmark = Bookmark.createYoutubeType(name, youtuberId, user);
 
 		// then
-		assertThat(bookmarks)
-			.hasSize(majorCategories.size())
-			.extracting(Bookmark::getMajorCategory)
-			.containsExactlyInAnyOrderElementsOf(majorCategories);
+		assertThat(bookmark.getBookmarkType()).isEqualTo(BookmarkType.YOUTUBE);
+	}
+
+	@DisplayName("Custom 즐겨찾기 생성 성공")
+	@Test
+	public void Custom_즐겨찾기_생성_성공() throws Exception {
+		// given
+		String name = "서촌";
+
+		// when
+		Bookmark bookmark = Bookmark.createCustomType(name, user);
+
+		// then
+		assertThat(bookmark.getBookmarkType()).isEqualTo(BookmarkType.CUSTOM);
+		assertThat(bookmark.getYoutuberId()).isNull();
 	}
 
 }
