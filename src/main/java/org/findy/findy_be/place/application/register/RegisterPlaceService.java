d1d8e79cb9996c349dc9db6200de9aa359c1a744
나경호 <hoyana1225@gmail.com>
2024-10-12 18:24:33 +0900

    refactor: BookmarkType에 따른 장소 저장 규칙 설정

--- src/main/java/org/findy/findy_be/place/application/register/RegisterPlaceService.java
+++ src/main/java/org/findy/findy_be/place/application/register/RegisterPlaceService.java
@@ -1,7 +1,10 @@
 package org.findy.findy_be.place.application.register;
 
+import static org.findy.findy_be.common.exception.ErrorMessage.*;
+
 import org.findy.findy_be.bookmark.application.find.FindBookMark;
 import org.findy.findy_be.bookmark.domain.Bookmark;
+import org.findy.findy_be.bookmark.domain.BookmarkType;
 import org.findy.findy_be.marker.application.create.CreateMarker;
 import org.findy.findy_be.place.application.find.FindPlace;
 import org.findy.findy_be.place.domain.Place;
@@ -27,16 +30,23 @@ public class RegisterPlaceService implements RegisterPlace {
 	@Override
 	public void invoke(PlaceRequest request) {
 		Bookmark bookmark = findBookMark.invokeById(request.bookmarkId());
-		Place place = findPlace.invoke(request).orElse(null);
-		if (isNull(place)) {
-			log.info("저장된 장소가 없어 새로운 장소를 저장합니다.");
-			place = createAndSaveNewPlace(request);
-		}
-		createMarker.invoke(bookmark, place);
+
+		validateBookmarkType(bookmark);
+
+		findPlace.invoke(request).ifPresentOrElse(
+			existingPlace -> createMarker.invoke(bookmark, existingPlace),
+			() -> {
+				log.info("저장된 장소가 없어 새로운 장소를 저장합니다.");
+				Place newPlace = createAndSaveNewPlace(request);
+				createMarker.invoke(bookmark, newPlace);
+			}
+		);
 	}
 
-	private static boolean isNull(final Place place) {
-		return place == null;
+	private void validateBookmarkType(Bookmark bookmark) {
+		if (bookmark.getBookmarkType().equals(BookmarkType.YOUTUBE)) {
+			throw new IllegalArgumentException(YOUTUBE_BOOKMARK_REGISTER_ERROR.getMessage());
+		}
 	}
 
 	private Place createAndSaveNewPlace(PlaceRequest request) {
