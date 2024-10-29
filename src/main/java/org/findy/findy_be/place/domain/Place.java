19ca7503e78407484960ee08e3f2f95ac7096e70
나경호 <hoyana1225@gmail.com>
2024-10-30 00:10:23 +0900

    refactor: Place 좌표 임베디드 타입 지정

--- src/main/java/org/findy/findy_be/place/domain/Place.java
+++ src/main/java/org/findy/findy_be/place/domain/Place.java
@@ -1,8 +1,11 @@
 package org.findy.findy_be.place.domain;
 
 import org.findy.findy_be.common.entity.BaseEntity;
+import org.findy.findy_be.place.domain.vo.Coordinate;
 import org.findy.findy_be.place.dto.request.PlaceRequest;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -33,36 +36,38 @@ public class Place extends BaseEntity {
 	private String telephone;
 
 	@NotNull
+	@Column(name = "address")
 	private String address;
 
 	@NotNull
+	@Column(name = "road_address")
 	private String roadAddress;
 
-	@NotNull
-	private String mapX;
-
-	@NotNull
-	private String mapY;
+	@Embedded
+	private Coordinate coordinate;
 
 	@Enumerated(EnumType.STRING)
 	@NotNull
+	@Column(name = "major_category")
 	private MajorCategory majorCategory;
 
 	@Enumerated(EnumType.STRING)
+	@Column(name = "middle_category")
 	private MiddleCategory middleCategory;
 
-	public static Place create(final PlaceRequest placeRequest) {
+	public static Place create(final PlaceRequest request) {
+		Coordinate coordinate = Coordinate.of(request.mapX(), request.mapY());
 		return Place.builder()
-			.address(placeRequest.address())
-			.description(placeRequest.description())
-			.link(placeRequest.link())
-			.majorCategory(placeRequest.majorCategory())
-			.mapX(placeRequest.mapX())
-			.mapY(placeRequest.mapY())
-			.middleCategory(placeRequest.middleCategory())
-			.roadAddress(placeRequest.roadAddress())
-			.telephone(placeRequest.telephone())
-			.title(placeRequest.title())
+			.address(request.address())
+			.description(request.description())
+			.link(request.link())
+			.majorCategory(request.majorCategory())
+			.coordinate(coordinate)
+			.middleCategory(request.middleCategory())
+			.roadAddress(request.roadAddress())
+			.telephone(request.telephone())
+			.title(request.title())
 			.build();
 	}
 }
+
---
+++ src/main/java/org/findy/findy_be/place/domain/vo/Coordinate.java
@@ -0,0 +1,42 @@
+package org.findy.findy_be.place.domain.vo;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coordinate {
+
+	@NotNull
+	private String mapX;
+
+	@NotNull
+	private String mapY;
+
+	public static Coordinate of(String mapX, String mapY) {
+		return new Coordinate(mapX, mapY);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Coordinate))
+			return false;
+		Coordinate that = (Coordinate)o;
+		return mapX.equals(that.mapX) && mapY.equals(that.mapY);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mapX, mapY);
+	}
+}
