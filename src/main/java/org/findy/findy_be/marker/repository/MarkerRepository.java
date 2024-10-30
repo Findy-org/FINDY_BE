0ffb627dc1889e7dcb1211e50e5dfe6f95b948c3
나경호 <hoyana1225@gmail.com>
2024-10-31 03:31:33 +0900

    refactor: bulk insert 마커 repository에 상속

--- src/main/java/org/findy/findy_be/marker/repository/MarkerRepository.java
+++ src/main/java/org/findy/findy_be/marker/repository/MarkerRepository.java
@@ -3,10 +3,11 @@ package org.findy.findy_be.marker.repository;
 import java.util.Optional;
 
 import org.findy.findy_be.bookmark.domain.Bookmark;
+import org.findy.findy_be.common.repository.BulkInsertRepository;
 import org.findy.findy_be.marker.domain.Marker;
 import org.findy.findy_be.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MarkerRepository extends JpaRepository<Marker, Long> {
+public interface MarkerRepository extends JpaRepository<Marker, Long>, BulkInsertRepository<Marker> {
 	Optional<Marker> findByBookmarkAndPlace(Bookmark bookmark, Place place);
 }
