3872e6d3f7af6a1dbc6dec8ba519f82417e6569e
나경호 <hoyana1225@gmail.com>
2024-10-31 03:32:36 +0900

    refactor: 장소 등록 api

--- src/main/java/org/findy/findy_be/place/api/PlaceController.java
+++ src/main/java/org/findy/findy_be/place/api/PlaceController.java
@@ -1,8 +1,11 @@
 package org.findy.findy_be.place.api;
 
+import org.findy.findy_be.common.meta.LoginUser;
 import org.findy.findy_be.place.api.swagger.PlaceAPIPresentation;
 import org.findy.findy_be.place.application.register.RegisterPlace;
-import org.findy.findy_be.place.dto.request.PlaceRequest;
+import org.findy.findy_be.place.dto.request.RegisterPlaceRequest;
+import org.findy.findy_be.user.domain.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +21,9 @@ public class PlaceController implements PlaceAPIPresentation {
 
 	private final RegisterPlace registerPlace;
 
-	@PostMapping()
-	public void registerPlace(@Valid @RequestBody PlaceRequest request) {
-		registerPlace.invoke(request);
+	@PostMapping("/{bookmarkId}")
+	public void registerPlace(@LoginUser User user, @PathVariable("bookmarkId") Long bookmarkId,
+		@Valid @RequestBody RegisterPlaceRequest request) {
+		registerPlace.invoke(user.getUserId(), request, bookmarkId);
 	}
 }
--- src/main/java/org/findy/findy_be/place/api/swagger/PlaceAPIPresentation.java
+++ src/main/java/org/findy/findy_be/place/api/swagger/PlaceAPIPresentation.java
@@ -2,10 +2,13 @@ package org.findy.findy_be.place.api.swagger;
 
 import org.findy.findy_be.common.meta.CustomApiResponse;
 import org.findy.findy_be.common.meta.CustomApiResponses;
-import org.findy.findy_be.place.dto.request.PlaceRequest;
+import org.findy.findy_be.common.meta.LoginUser;
+import org.findy.findy_be.place.dto.request.RegisterPlaceRequest;
+import org.findy.findy_be.user.domain.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -18,9 +21,10 @@ public interface PlaceAPIPresentation {
 		@ApiResponse(responseCode = "400", description = "유효하지 않은 입력 값")
 	})
 	@CustomApiResponses({
-		@CustomApiResponse(error = "HttpMessageNotReadableException", status = 400, message = "Validation failed for argument [0] in public void org.findy.findy_be.place.api.PlaceController.registerPlace(org.findy.findy_be.place.dto.request.PlaceRequest) with 7 errors: [Field error in object 'placeRequest' on field 'link': rejected value [null]; codes [NotNull.placeRequest.link,NotNull.link,NotNull.java.lang.String,NotNull]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [placeRequest.link,link]; arguments []; default message [link]]; default message [널이어서는 안됩니다]] [Field error in object 'placeRequest' on field 'address': rejected value [null]; codes [NotNull.placeRequest.address,NotNull.address,NotNull.java.lang.String,NotNull]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [placeRequest.address,address]; arguments []; default message [address]]; default message [널이어서는 안됩니다]]", description = "validation에 맞지 않은 요청을 할 경우"),
+		@CustomApiResponse(error = "HttpMessageNotReadableException", status = 400, message = "유튜브 즐겨찾기는 장소를 추가할 수 없습니다.", description = "validation에 맞지 않은 요청을 할 경우"),
 		@CustomApiResponse(error = "EntityNotFoundException", status = 404, message = "해당 id : 1의 즐겨찾기가 존재하지 않습니다.", description = "장소를 저장할 즐겨찾기를 못 찾는 경우"),
 		@CustomApiResponse(error = "InternalServerError", status = 500, message = "내부 서버 오류가 발생했습니다.", description = "내부 서버 오류")
 	})
-	void registerPlace(@Valid @RequestBody PlaceRequest request);
+	void registerPlace(@LoginUser User user, @PathVariable Long bookmarkId,
+		@Valid @RequestBody RegisterPlaceRequest request);
 }
