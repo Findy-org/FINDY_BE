cb579ecd77645ec9d78dd4971312aaac4131f060
나경호 <hoyana1225@gmail.com>
2024-10-31 03:26:38 +0900

    feat: 유튜브 즐겨찾기 저장 request dto

---
+++ src/main/java/org/findy/findy_be/bookmark/dto/request/CategoryRequest.java
@@ -0,0 +1,17 @@
+package org.findy.findy_be.bookmark.dto.request;
+
+import org.findy.findy_be.place.domain.MajorCategory;
+import org.findy.findy_be.place.domain.MiddleCategory;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record CategoryRequest(
+	@NotNull(message = "대분류는 비어있을 수 없습니다.")
+	@Schema(description = "대분류", example = "RESTAURANT")
+	MajorCategory majorCategory,
+
+	@Schema(description = "중분류", example = "KOREAN")
+	MiddleCategory middleCategory
+) {
+}
---
+++ src/main/java/org/findy/findy_be/bookmark/dto/request/YoutubeBookmarkRequest.java
@@ -0,0 +1,30 @@
+package org.findy.findy_be.bookmark.dto.request;
+
+import java.util.List;
+
+import org.findy.findy_be.common.validation.ValidYoutuberId;
+import org.findy.findy_be.place.dto.request.RegisterPlaceRequest;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "유튜브 즐겨찾기 DTO")
+public record YoutubeBookmarkRequest(
+
+	@ValidYoutuberId(message = "유튜버 ID는 @으로 시작해야합니다.")
+	@Schema(description = "유튜버 ID", example = "@iammingki")
+	String youtuberId,
+
+	@NotNull(message = "유튜버 이름은 비어있을 수 없습니다.")
+	@Schema(description = "유튜버 이름", example = "걍밍경")
+	String youtuberName,
+
+	@Schema(description = "유튜버 프로필 link", example = "https://yt3.googleusercontent.com/ytc/AIdro_mieTH2WSE4oBMmczfLHB3HhikzOg1nz9tFD-MLad93Xnw=s160-c-k-c0x00ffffff-no-rj")
+	String youtuberProfile,
+
+	@Schema(description = "유튜브 링크", example = "https://www.youtube.com/watch?v=hE2wMo5Coco")
+	String youtubeLink,
+
+	List<RegisterPlaceRequest> selectedPlaces
+) {
+}
