7f6bbff8240e3b232f5359b404c4d8bcb495a2f0
나경호 <hoyana1225@gmail.com>
2024-10-31 11:55:30 +0900

    refactor: marker select 쿼리 호출 수 단건으로 최적화
    marker 증가 시 Bookmark 수량 증가

--- src/test/java/org/findy/findy_be/place/application/register/BatchRegisterPlaceServiceTest.java
+++ src/test/java/org/findy/findy_be/place/application/register/BatchRegisterPlaceServiceTest.java
@@ -91,7 +91,7 @@ class BatchRegisterPlaceServiceTest extends MockTest {
 		verify(batchCreateMarker, times(1)).invoke(eq(testBookmark),
 			argThat(list -> list.contains(place1) && list.contains(place2)));
 	}
-	
+
 	@DisplayName("단일 장소가 등록되고 마커가 생성됨")
 	@Test
 	void 단일_장소_등록_및_마커_생성() {
@@ -108,4 +108,46 @@ class BatchRegisterPlaceServiceTest extends MockTest {
 		verify(placeRepository, times(1)).bulkInsert(anyList());
 		verify(batchCreateMarker, times(1)).invoke(testBookmark, Arrays.asList(place1));
 	}
+
+	@DisplayName("새로운 장소가 모두 등록되고 장소 수가 업데이트됨")
+	@Test
+	void 새로운_장소_모두_등록_및_장소_수_업데이트() {
+		// given
+		List<RegisterPlaceRequest> requests = Arrays.asList(request1, request2);
+
+		when(findPlace.invoke(request1.title(), request1.roadAddress())).thenReturn(Optional.empty());
+		when(findPlace.invoke(request2.title(), request2.roadAddress())).thenReturn(Optional.empty());
+		when(placeRepository.bulkInsert(anyList())).thenReturn(Arrays.asList(place1, place2));
+
+		// when
+		batchRegisterPlaceService.invoke(testBookmark, requests);
+
+		// then
+		verify(placeRepository, times(1)).bulkInsert(anyList());
+		verify(batchCreateMarker, times(1)).invoke(testBookmark, Arrays.asList(place1, place2));
+
+		verify(testBookmark, times(1)).incrementMarkersCount(2);
+	}
+
+	@DisplayName("일부 장소가 이미 등록되어 새로 등록된 장소 수가 업데이트됨")
+	@Test
+	void 일부_장소_이미_등록되어_새로운_장소_수_업데이트() {
+		// given
+		List<RegisterPlaceRequest> requests = Arrays.asList(request1, request2);
+
+		when(findPlace.invoke(request1.title(), request1.roadAddress())).thenReturn(Optional.empty());
+		when(findPlace.invoke(request2.title(), request2.roadAddress())).thenReturn(Optional.of(place2));
+		when(placeRepository.bulkInsert(anyList())).thenReturn(Arrays.asList(place1));
+
+		// when
+		batchRegisterPlaceService.invoke(testBookmark, requests);
+
+		// then
+		verify(placeRepository, times(1)).bulkInsert(anyList());
+		verify(batchCreateMarker, times(1)).invoke(eq(testBookmark),
+			argThat(list -> list.contains(place1) && list.contains(place2)));
+
+		verify(testBookmark, times(1)).incrementMarkersCount(1);
+	}
+
 }
