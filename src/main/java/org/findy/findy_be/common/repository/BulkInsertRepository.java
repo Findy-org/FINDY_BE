6a161a48b8cb35a91728634c0882b3d7ce72ee40
나경호 <hoyana1225@gmail.com>
2024-11-03 23:03:35 +0900

    refactor: bulk insert 적용

--- src/main/java/org/findy/findy_be/common/repository/BulkInsertRepository.java
+++
@@ -1,7 +0,0 @@
-package org.findy.findy_be.common.repository;
-
-import java.util.List;
-
-public interface BulkInsertRepository<T> {
-	List<T> bulkInsert(List<T> entities);
-}
--- src/main/java/org/findy/findy_be/place/repository/PlaceRepository.java
+++ src/main/java/org/findy/findy_be/place/repository/PlaceRepository.java
@@ -2,13 +2,12 @@ package org.findy.findy_be.place.repository;
 
 import java.util.Optional;
 
-import org.findy.findy_be.common.repository.BulkInsertRepository;
 import org.findy.findy_be.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PlaceRepository extends JpaRepository<Place, Long>, BulkInsertRepository<Place> {
+public interface PlaceRepository extends JpaRepository<Place, Long> {
 
 	@Query("SELECT p FROM Place p WHERE p.title = :title AND p.roadAddress = :roadAddress")
 	Optional<Place> findPlaceByDetails(@Param("title") String title, @Param("roadAddress") String roadAddress);
