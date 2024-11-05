105ad9754f8f1d04b8f41abc839cbbc9ea9c5e9c
나경호 <hoyana1225@gmail.com>
2024-11-06 03:09:59 +0900

    feat: Bookmark find all data 페이지네이션

---
+++ src/main/java/org/findy/findy_be/bookmark/application/find/FindAllPagedBookmarks.java
@@ -0,0 +1,8 @@
+package org.findy.findy_be.bookmark.application.find;
+
+import org.findy.findy_be.bookmark.dto.response.BookmarkResponse;
+import org.findy.findy_be.common.dto.pagination.SliceResponse;
+
+public interface FindAllPagedBookmarks {
+	SliceResponse<BookmarkResponse> invoke(final String userId, final Long cursor, final int size);
+}
