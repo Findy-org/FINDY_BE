ca2922d145516f9c2df046e73137a4d39b09659d
나경호 <hoyana1225@gmail.com>
2024-11-06 03:05:35 +0900

    feat: Bookmark 페이지네이션 list controller

--- src/main/java/org/findy/findy_be/bookmark/api/BookmarkController.java
+++ src/main/java/org/findy/findy_be/bookmark/api/BookmarkController.java
@@ -1,10 +1,16 @@
 package org.findy.findy_be.bookmark.api;
 
 import org.findy.findy_be.bookmark.api.swagger.BookmarkAPIPresentation;
+import org.findy.findy_be.bookmark.application.find.FindAllPagedBookmarks;
 import org.findy.findy_be.bookmark.application.register.RegisterYoutubeBookmark;
+import org.findy.findy_be.bookmark.dto.request.PagedBookmarkRequest;
 import org.findy.findy_be.bookmark.dto.request.YoutubeBookmarkRequest;
+import org.findy.findy_be.bookmark.dto.response.BookmarkResponse;
+import org.findy.findy_be.common.dto.pagination.SliceResponse;
 import org.findy.findy_be.common.meta.LoginUser;
 import org.findy.findy_be.user.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +25,16 @@ import lombok.RequiredArgsConstructor;
 public class BookmarkController implements BookmarkAPIPresentation {
 
 	private final RegisterYoutubeBookmark registerYoutubeBookmark;
+	private final FindAllPagedBookmarks findAllPagedBookmarks;
 
 	@PostMapping("/youtube")
 	public void registerPlace(@LoginUser User user, @Valid @RequestBody YoutubeBookmarkRequest request) {
 		registerYoutubeBookmark.invoke(user.getUserId(), request);
+	}
+
+	@GetMapping
+	public SliceResponse<BookmarkResponse> getBookmarkList(@LoginUser User user,
+		@ModelAttribute PagedBookmarkRequest request) {
+		return findAllPagedBookmarks.invoke(user.getUserId(), request.cursor(), request.size());
 	}
 }
