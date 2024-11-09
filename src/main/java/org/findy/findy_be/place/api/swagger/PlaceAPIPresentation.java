6a6716d0f487f4ee63cbe25edb75cbb7190e7d5f
나경호 <hoyana1225@gmail.com>
2024-11-09 22:44:52 +0900

    feat: 장소 조회 API Swagger에 추가

--- src/main/java/org/findy/findy_be/place/api/swagger/PlaceAPIPresentation.java
+++ src/main/java/org/findy/findy_be/place/api/swagger/PlaceAPIPresentation.java
@@ -1,10 +1,14 @@
 package org.findy.findy_be.place.api.swagger;
 
+import org.findy.findy_be.common.dto.pagination.request.PagedRequest;
+import org.findy.findy_be.common.dto.pagination.response.SliceResponse;
 import org.findy.findy_be.common.meta.CustomApiResponse;
 import org.findy.findy_be.common.meta.CustomApiResponses;
 import org.findy.findy_be.common.meta.LoginUser;
-import org.findy.findy_be.place.dto.request.RegisterPlaceRequest;
+import org.findy.findy_be.place.dto.request.RegisterSearchedPlaceRequest;
+import org.findy.findy_be.place.dto.response.PlaceResponse;
 import org.findy.findy_be.user.domain.User;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,5 +30,15 @@ public interface PlaceAPIPresentation {
 		@CustomApiResponse(error = "InternalServerError", status = 500, message = "내부 서버 오류가 발생했습니다.", description = "내부 서버 오류")
 	})
 	void registerPlace(@LoginUser User user, @PathVariable Long bookmarkId,
-		@Valid @RequestBody RegisterPlaceRequest request);
+		@Valid @RequestBody RegisterSearchedPlaceRequest request);
+
+	@Operation(summary = "장소 목록 조회", description = "유저의 북마크 장소 목록을 조회하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "장소 목록 조회 성공")
+	})
+	@CustomApiResponses({
+		@CustomApiResponse(error = "IllegalArgumentException", status = 400, message = "잘못된 요청입니다.", description = "잘못된 쿼리 파라미터가 포함된 경우"),
+		@CustomApiResponse(error = "InternalServerError", status = 500, message = "내부 서버 오류가 발생했습니다.", description = "서버 내부에서 예기치 않은 오류가 발생한 경우")
+	})
+	SliceResponse<PlaceResponse> getPlaces(@LoginUser User user, @PathVariable("bookId") Long bookId,
+		@ModelAttribute PagedRequest request);
 }
