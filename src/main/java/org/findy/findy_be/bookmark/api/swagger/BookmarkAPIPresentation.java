3ad63e9f65cd2419591042b3ada06c8b9acdaac7
나경호 <hoyana1225@gmail.com>
2024-11-16 22:01:01 +0900

    feat: Bookmark 삭제 Swagger API

--- src/main/java/org/findy/findy_be/bookmark/api/swagger/BookmarkAPIPresentation.java
+++ src/main/java/org/findy/findy_be/bookmark/api/swagger/BookmarkAPIPresentation.java
@@ -10,6 +10,7 @@ import org.findy.findy_be.common.meta.CustomApiResponses;
 import org.findy.findy_be.common.meta.LoginUser;
 import org.findy.findy_be.user.domain.User;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -51,4 +52,15 @@ public interface BookmarkAPIPresentation {
 		@CustomApiResponse(error = "InternalServerError", status = 500, message = "내부 서버 오류가 발생했습니다.", description = "서버 내부에서 예기치 않은 오류가 발생한 경우")
 	})
 	SliceResponse<BookmarkResponse> getBookmarkList(@LoginUser User user, @ModelAttribute PagedRequest request);
+
+	@Operation(summary = "북마크 삭제", description = "북마크를 삭제하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "북마크 삭제 성공")
+	})
+	@CustomApiResponses({
+		@CustomApiResponse(error = "IllegalArgumentException", status = 400, message = "잘못된 요청입니다.", description = "잘못된 쿼리 파라미터가 포함된 경우"),
+		@CustomApiResponse(error = "ForbiddenAccessException", status = 403, message = "해당 즐겨찾기에 접근할 권한이 없습니다.", description = "권한이 없는 유저가 즐겨찾기에 접근할 경우"),
+		@CustomApiResponse(error = "EntityNotFoundException", status = 404, message = "해당 id : {id}의 즐겨찾기가 존재하지 않습니다.", description = "존재하지 않는 즐겨찾기에 접근할 경우"),
+		@CustomApiResponse(error = "InternalServerError", status = 500, message = "내부 서버 오류가 발생했습니다.", description = "서버 내부에서 예기치 않은 오류가 발생한 경우")
+	})
+	void deleteBookmark(@LoginUser User user, @PathVariable("id") Long bookmarkId);
 }
