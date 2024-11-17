6562301f2381d38ed2278f64d89574bdac72b438
나경호 <hoyana1225@gmail.com>
2024-11-17 14:07:13 +0900

    refactor: marker refactoring

--- src/main/java/org/findy/findy_be/marker/application/register/PlaceWithTimestamp.java
+++
@@ -1,9 +0,0 @@
-package org.findy.findy_be.marker.application.register;
-
-import org.findy.findy_be.place.domain.Place;
-
-public record PlaceWithTimestamp(
-	Place place,
-	String timestamp
-) {
-}
