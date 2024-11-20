c93c5509805629011dbc4ff0f341ef6638ee520b
나경호 <hoyana1225@gmail.com>
2024-11-20 18:13:03 +0900

    refactor: marker의 좌표와 bookmark name도 보내도록 변경

--- src/main/java/org/findy/findy_be/marker/application/find/FindAllPagedMarkersService.java
+++ src/main/java/org/findy/findy_be/marker/application/find/FindAllPagedMarkersService.java
@@ -1,8 +1,13 @@
 package org.findy.findy_be.marker.application.find;
 
+import static org.findy.findy_be.common.exception.ErrorCode.*;
+
 import java.util.List;
 
+import org.findy.findy_be.bookmark.domain.Bookmark;
+import org.findy.findy_be.bookmark.repository.BookmarkRepository;
 import org.findy.findy_be.common.dto.pagination.response.SliceResponse;
+import org.findy.findy_be.marker.dto.response.PagedMarkerResponse;
 import org.findy.findy_be.place.dto.response.MarkerPlaceResponse;
 import org.findy.findy_be.place.repository.PlaceRepository;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +16,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,9 +25,16 @@ import lombok.RequiredArgsConstructor;
 public class FindAllPagedMarkersService implements FindAllPagedMarkers {
 
 	private final PlaceRepository placeRepository;
+	private final BookmarkRepository bookmarkRepository;
 
-	public SliceResponse<MarkerPlaceResponse> invoke(final String userId, final Long bookmarkId, final Long cursor,
+	public PagedMarkerResponse invoke(final String userId, final Long bookmarkId, final Long cursor,
 		final int size) {
+
+		Bookmark bookmark = bookmarkRepository.findById(bookmarkId)
+			.orElseThrow(
+				() -> new EntityNotFoundException(String.format(NOT_FOUND_BOOKMARK_BY_ID.getMessage(), bookmarkId)));
+		;
+
 		Pageable pageable = PageRequest.of(0, size);
 		Slice<MarkerPlaceResponse> placeSlice = placeRepository.findPlacesByUserIdAndBookmarkId(userId, bookmarkId,
 			pageable, cursor);
@@ -29,7 +42,9 @@ public class FindAllPagedMarkersService implements FindAllPagedMarkers {
 		List<MarkerPlaceResponse> data = placeSlice.getContent();
 		Long nextCursor = placeSlice.hasNext() ? data.get(data.size() - 1).markerId() : null;
 
-		return new SliceResponse<>(data, cursor != null ? cursor.intValue() : 0, size, placeSlice.hasNext(),
+		SliceResponse<MarkerPlaceResponse> sliceResponse = new SliceResponse<>(data,
+			cursor != null ? cursor.intValue() : 0, size, placeSlice.hasNext(),
 			nextCursor);
+		return PagedMarkerResponse.of(bookmark.getName(), sliceResponse);
 	}
 }
