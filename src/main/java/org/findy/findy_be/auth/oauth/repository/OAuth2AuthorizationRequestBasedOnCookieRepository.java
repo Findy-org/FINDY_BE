174419286ba241bbc2f6544e5287f668d7b414c1
나경호 <hoyana1225@gmail.com>
2024-11-29 09:22:13 +0900

    refactor: orgin 성공시 redirect

--- src/main/java/org/findy/findy_be/auth/oauth/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/org/findy/findy_be/auth/oauth/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -51,8 +51,9 @@ public class OAuth2AuthorizationRequestBasedOnCookieRepository
 
 	public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
 		CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+		CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
 		CookieUtil.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
-		CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+		CookieUtil.deleteCookie(request, response, ACCESS_TOKEN);
 	}
 }
 
